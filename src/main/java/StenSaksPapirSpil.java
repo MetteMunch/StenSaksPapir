230d82715ed5974f94245a655ede21ffaec8cadb
MetteMunch <meha0005@stud.kea.dk>
2024-04-18 21:56:02 +0200

    Færdiggørelse af spil med korrekt sammentælling og afslutning

--- src/main/java/StenSaksPapirSpil.java
+++ src/main/java/StenSaksPapirSpil.java
@@ -15,59 +15,50 @@ public class StenSaksPapirSpil {
         Håndtegn[] håndtegnArray = Håndtegn.values();
         int spiller1Håndtegn = 0;
         int spiller2Håndtegn = 0;
-        int SENTINEL = 0;
+        // int SENTINEL = 0;
         int spilleRunde = 0;
-        Spiller vinner;
+        //Spiller vinner;
 
 
-        while (SENTINEL != 5) {
-            System.out.println(spiller1Håndtegn = spiller1.vælgHåndtegn());
+        while (true) {
+            spiller1Håndtegn = spiller1.vælgHåndtegn();
             spiller2Håndtegn = spiller2.vælgHåndtegn();
+
+            //System.out.println("spiller1Håndtegn: " + spiller1Håndtegn); //test af træk
+            //System.out.println("spiller2Håndtegn: " + spiller2Håndtegn); //test af træk
+
 
             if (spiller1Håndtegn == spiller2Håndtegn) { //ens håndtegn
                 System.out.println("Ens håndtegn, det er en ommer!");
                 System.out.println(" ");//ekstra linjeskift
-            } else if (spiller1Håndtegn == 1 && spiller2Håndtegn == 2) { //sten mod saks
-                spilleRunde++;
+            } else if ((spiller1Håndtegn == 1 && spiller2Håndtegn == 2) ||
+                    (spiller1Håndtegn == 2 && spiller2Håndtegn == 3) ||
+                    (spiller1Håndtegn == 3 && spiller2Håndtegn == 1)) {
+                System.out.println("Runde " + (++spilleRunde) + ", " + navnSpiller1 + " trækker: " + håndtegnArray[spiller1Håndtegn-1] + " " + navnSpiller2 + "trækker: " + håndtegnArray[spiller2Håndtegn-1]);
+                System.out.println(navnSpiller1 + " vinder!");
                 spiller1.sammentællingPoint();
-            } else if (spiller1Håndtegn == 2 && spiller2Håndtegn == 1) { //saks mod sten
-                spilleRunde++;
+            } else {
+                System.out.println("Runde " + (++spilleRunde) + ", " + navnSpiller1 + " trækker: " + håndtegnArray[spiller1Håndtegn-1] + " " + navnSpiller2 + "trækker: " + håndtegnArray[spiller2Håndtegn-1]);
+                System.out.println(navnSpiller2 + " vinder!");
                 spiller2.sammentællingPoint();
-            } else if (spiller1Håndtegn == 3 && spiller2Håndtegn == 2) { //papir mod saks
-                spilleRunde++;
-                spiller2.sammentællingPoint();
-            } else if (spiller1Håndtegn == 1 && spiller2Håndtegn == 3) { //sten mod papir
-                spilleRunde++;
-                spiller2.sammentællingPoint();
-            } else if (spiller1Håndtegn == 2 && spiller2Håndtegn == 3) { //saks mod papir
-                spilleRunde++;
-                spiller1.sammentællingPoint();
-            } else if (spiller1Håndtegn == 3 && spiller2Håndtegn == 1) { //papir mod sten
-                spilleRunde++;
-                spiller1.sammentællingPoint();
+
             }
             System.out.println(" ");//ekstra linjeskift
-            System.out.println("Rundens resultat: ");
-            System.out.println("Spiller: " + navnSpiller1);
-            System.out.println("Valgte: " + håndtegnArray[spiller1Håndtegn-1]);
-            System.out.println("Point spiller 1: ");
-            System.out.println(spiller1.getSpillerPoint());
-            System.out.println("Spiller: " + navnSpiller2);
-            System.out.println("Valgte: " + håndtegnArray[spiller2Håndtegn-1]);
-            System.out.println("Point spiller 2: ");
-            System.out.println(spiller2.getSpillerPoint());
+            System.out.println("Point: ");
+            System.out.println(navnSpiller1 + ": " + spiller1.getSpillerPoint());
+            System.out.println(navnSpiller2 + ": " + spiller2.getSpillerPoint());
             System.out.println(" "); //ekstra linjeskift
 
-            if (spiller1.getSpillerPoint() == 2) {
-                System.out.println("Juhu vindenren er: " + spiller1.getSpillerNavn());
-                SENTINEL = 5;
-            } else if (spiller2.getSpillerPoint() == 2) {
-                System.out.println("Juhu vindenren er: " + spiller1.getSpillerNavn());
-                SENTINEL = 5;
+            if (spiller1.getSpillerPoint() == 2 ||spiller2.getSpillerPoint() == 2){
+                if (spiller1.getSpillerPoint() == 2) {
+                    System.out.println("Juhu vinderen er: " + navnSpiller1);
+                } else {
+                    System.out.println("Juhu vindenren er: " + navnSpiller2);
+                }
+                break;
             }
 
         }
-        System.out.println("udprint efter begge if elser");
 
     }
 }
